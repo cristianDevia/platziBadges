c72136b55020db80d6c6f9fe8fa8e24604f31521
Cristian Devia <c.devia8@gmail.com>
2021-06-29 20:17:31 -0500

    Index inicial, se crea la carpeta de componentes y el archivo badges.js

--- src/index.js
+++ src/index.js
@@ -17,19 +17,27 @@ const sum = () => 9 + 9;
 // const element = React.createElement('h1', {}, `Hola, soy ${name}`);
 //const jsx = <h1>Hola, soy {sum()}</h1>;
 
-const jsx = <div>
-    <h1> Hola, soy Cristian</h1>
-    <p>
-        Soy estudiante de 10 semestre
-    </p>
-</div>
+// const jsx = <div>
+//     <h1> Hola, soy Cristian</h1>
+//     <p>
+//         Soy estudiante de 10 semestre
+//     </p>
+// </div>
 
-const element = React.createElement(
-    'div', {}, React.createElement('h1', {}, 'Hola, soy Daniel'),
-    React.createElement('p', {}, 'Soy estudiante de ingeniera de sistemas')
+// const element = React.createElement(
+//     'div', {}, React.createElement('h1', {}, 'Hola, soy Daniel'),
+//     React.createElement('p', {}, 'Soy estudiante de ingeniera de sistemas')
+// );
+// const container = document.getElementById('app');
+
+
+const element = (
+    <div>
+        <h1>Hola, Soy Cristian</h1>
+        <p>soy estudiante de ingenieria</p>
+    </div>
 );
 const container = document.getElementById('app');
-
 
 //ReactDOM.render(__qué__, ___dónde___);
 ReactDOM.render(element, container);
