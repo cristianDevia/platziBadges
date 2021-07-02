eaff0edf338c1e8e81bcc15bc767a054ec5997ee
Cristian Devia <c.devia8@gmail.com>
2021-07-02 18:48:52 -0500

    se agrega la carpeta de imagenes, se installa bootstrap sin npm, se configuran estilos global y badge

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,26 +1,27 @@
 import React from 'react';
+import "../styles/Badge.css"
 import confLogo from '../images/badge-header.svg'
 
 class Badge extends React.Component {
 
     render() {
         return (
-            <div>
-                <div>
+            <div className="Badge">
+                <div className="Badge__header">
                     <img src={confLogo} alt="Logo de la conferencia"></img>
                 </div>
 
-                <div>
-                    <img src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar"></img>
+                <div className="Badge__section-name">
+                    <img className="Badge__avatar" src="https://s.gravatar.com/avatar/478892551ca54e1ddbb7584fa8b1643a?s=80" alt="Avatar"></img>
                     <h1> Cristian <br></br> Devia </h1>
                 </div>
 
-                <div>
-                    <p>FrontEnd Engineer</p>
-                    <p>@Aquelarre</p>
+                <div className="Badge__section-info">
+                    <h3>FrontEnd Engineer</h3>
+                    <div>@Aquelarre</div>
                 </div>
 
-                <div>
+                <div className="Badge__footer">
                     #PlatziConf
                 </div>
 
--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "bootstrap/dist/css/bootstrap.css"
+import "./global.css"
 import Badge from './components/Badge';
 
 //const jsx = <h1>Hello, Platzi Badges</h1>;
